f265f1d1d8b459efcb38a7888119b03923ecb6d5
sswagg <seventeenman@qq.com>
2022-04-01 22:38:12 +0800

    release

---
+++ package.sh
@@ -0,0 +1,7 @@
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o ./release/aScan_linux64
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags="-s -w" -o ./release/aScan_win64 ./main.go
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -ldflags="-s -w" -o ./release/aScan_darwin ./main.go
+CGO_ENABLED=0 GOOS=linux GOARCH=386 go build -ldflags="-s -w" -o ./release/aScan_linux32
+CGO_ENABLED=0 GOOS=windows GOARCH=386 go build -ldflags="-s -w" -o ./release/aScan_win32 ./main.go
+CGO_ENABLED=0 GOOS=linux GOARCH=arm go build -ldflags="-s -w" -o ./release/aScan_linuxarm
+CGO_ENABLED=0 GOOS=windows GOARCH=arm go build -ldflags="-s -w" -o ./release/aScan_winarm ./main.go
